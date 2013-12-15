921cc611431a76ec3a15f00f58657e6548cff5a6
mhallin <mhallin@gmail.com>
2013-12-15 15:26:57 +0100

    Generic random functionality in math_utils

--- include/rosewood/math/math_utils.h
+++ include/rosewood/math/math_utils.h
@@ -1,6 +1,7 @@
 #ifndef __ROSEWOOD_MATH_MATH_UTILS_H__
 #define __ROSEWOOD_MATH_MATH_UTILS_H__
 
+#include <stdlib.h>
 #include <algorithm>
 #include <cmath>
 
@@ -14,6 +15,18 @@ namespace rosewood { namespace math {
     template<typename T>
     T sign(T value) {
         return std::copysign(T(1), value);
+    }
+
+    template<typename T> T random(T min, T max);
+
+    template<>
+    float random(float min, float max) {
+        return min + (max - min) * (static_cast<double>(::random()) / static_cast<double>(INT_MAX));
+    }
+
+    template<>
+    Vector3 random(Vector3 min, Vector3 max) {
+        return Vector3(random(min.x(), max.x()), random(min.y(), max.y()), random(min.z(), max.z()));
     }
 
 } }
