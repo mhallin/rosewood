f90ae9c2eb392ba2e0901f6f4668545e097b5929
mhallin <mhallin@gmail.com>
2014-04-21 13:53:50 +0200

    Improved randomisation code

--- engine/math/include/rosewood/math/math_utils.h
+++ engine/math/include/rosewood/math/math_utils.h
@@ -17,16 +17,16 @@ namespace rosewood { namespace math {
         return std::copysign(T(1), value);
     }
 
-    template<typename T> T random(T min, T max);
-
-    template<>
-    float random(float min, float max) {
+    inline float random(float min, float max) {
         return min + (max - min) * (static_cast<double>(::random()) / static_cast<double>(INT_MAX));
     }
 
-    template<>
-    Vector3 random(Vector3 min, Vector3 max) {
+    inline Vector3 random(Vector3 min, Vector3 max) {
         return Vector3(random(min.x(), max.x()), random(min.y(), max.y()), random(min.z(), max.z()));
+    }
+
+    template<typename T> T random_centered(T center, T diff) {
+        return random(center - diff, center + diff);
     }
 
 } }
